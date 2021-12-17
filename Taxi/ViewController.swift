46e93c62e0ea1ecb7d311e2a8791e64581b2a472
SiddikjonKuziboev <kuziboevsiddiqjon98@gmail.com>
2021-12-17 07:15:13 +0500

    Initial Commit

---
+++ Taxi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Taxi
+//
+//  Created by Kuziboev Siddikjon on 12/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
